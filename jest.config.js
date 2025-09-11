30da933c4b9dcd43f31c09dcbf67a1dc462a98f0
hossainks <shakirhossain.qa@gmail.com>
2025-09-11 13:28:29 +0600

    Generate more data on coverage summary

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,15 @@
 module.exports = {
     testEnvironment: 'node',
     testMatch: ['**/tests/**/*.spec.js'],
+    
+    // Coverage configuration
     collectCoverage: true,
     coverageDirectory: 'coverage',
+    coverageReporters: ['text-summary', 'text', 'html'],
+
+    // Reporters configuration
     reporters: [
-        'default',
+        'default', 
         ['jest-junit', {
             outputDirectory: './reports',
             outputName: 'junit.xml'
