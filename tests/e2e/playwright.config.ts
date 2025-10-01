f2b0f5eff87105a60494ed2195dc01b5fef643b9
hossainks <shakirhossain.qa@gmail.com>
2025-10-01 12:51:33 +0600

    Add e2e tests

---
+++ tests/e2e/playwright.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? undefined : undefined,
+  reporter: 'html',
+  use: {
+    baseURL: 'https://fast.com/',
+    trace: 'on-first-retry',
+  },
+  timeout: 60 * 1000,
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+  ],
+});
